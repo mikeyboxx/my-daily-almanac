dcfe95d6e4b80218ffb85b5a9c4e4781485caec3
AKJ <kjajila@gmail.com>
2022-10-12 13:57:38 -0400

    add eventlistner

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var navBtn=document.getElementById("btn");
+navBtn.addEventListener("click",function(){
+    //alert("testing");
+    var m=document.getElementById("menu");
+    m.classList.add("navbar-menu");
+    //console.log(m.children[1].children[2]);
+    //m.display=true;
+});

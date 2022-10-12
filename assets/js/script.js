b1cd1cce922959f3c5e9b7fd8ff29045dbed1474
AKJ <kjajila@gmail.com>
2022-10-12 13:54:12 -0400

    initial commit

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-var navBtn=document.getElementById("btn");
-navBtn.addEventListener("click",function(){
-    //alert("testing");
-    var m=document.getElementById("menu");
-    m.classList.add("navbar-menu");
-    //console.log(m.children[1].children[2]);
-    //m.display=true;
-});

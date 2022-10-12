d4757467a864e6c31049c6cef2cde6a0c55c7b35
AKJ <kjajila@gmail.com>
2022-10-12 16:20:46 -0400

    commit after testing and add comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,13 @@
+// varaibles
 var navBtn=document.getElementById("btn");
 var menu = document.getElementById("menu");
 
-
+// eventlistner for hamburgur menu
 navBtn.addEventListener("click",function(){
-     var menu = document.getElementById("menu");
-    // (menu.children[1]).display=true;
-    //alert("testing");
-    //navBtn.classList.add("navbar-item","is-hoverable","navbar-dropdown","navbar-item");
-    //navBtn.classList.add("navbar-item","is-hoverable");
-    //console.log(menu.children[1].children[2]);
-    if(menu.classList.contains("showMenu"))
-    {
-     menu.classList.remove("showMenu");
+    if(menu.classList.contains("showMenu")){
+          menu.classList.remove("showMenu");//remove the class showmenu from menu element
     }
     else {
-      menu.classList.add("showMenu");
+          menu.classList.add("showMenu");//add the class showmenu to menu element
     }
-
-    
 });

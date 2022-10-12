aebd9c97791826ad6e9a79a1804a43c027bc1957
Michael Nabatov <happyanddebtfree@gmail.com>
2022-10-12 15:03:28 -0400

    Added horsocope api

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,21 @@ async function bigDataCloudApi(obj){
         obj.longitude = resp.longitude;  
 }
 
+
+async function aztroAPi(sign, day){
+    let resp = {};
+    const options = {
+        method: 'POST',
+        headers: {
+            'X-RapidAPI-Key': '7e3d61519emsh748d639db16a48fp154cddjsn756669cf1b3e',
+            'X-RapidAPI-Host': 'sameer-kumar-aztro-v1.p.rapidapi.com'
+        }
+    };
+    await fetch(`https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=${sign}&day=${day}`, options)
+        .then(response => response.json())
+        .then(response => {console.log(response); resp = response})
+        .catch(err => {console.error(err); return err});
+};
+
+aztroAPi();
+

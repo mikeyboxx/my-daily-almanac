205b9a01ddc34eccfd8546b620d394cf6e077610
AKJ <kjajila@gmail.com>
2022-10-12 16:12:55 -0400

    add and remove class

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,21 @@
 var navBtn=document.getElementById("btn");
+var menu = document.getElementById("menu");
+
+
 navBtn.addEventListener("click",function(){
+     var menu = document.getElementById("menu");
+    // (menu.children[1]).display=true;
     //alert("testing");
-    var m=document.getElementById("menu");
-    m.classList.add("navbar-menu");
-    //console.log(m.children[1].children[2]);
-    //m.display=true;
+    //navBtn.classList.add("navbar-item","is-hoverable","navbar-dropdown","navbar-item");
+    //navBtn.classList.add("navbar-item","is-hoverable");
+    //console.log(menu.children[1].children[2]);
+    if(menu.classList.contains("showMenu"))
+    {
+     menu.classList.remove("showMenu");
+    }
+    else {
+      menu.classList.add("showMenu");
+    }
+
+    
 });

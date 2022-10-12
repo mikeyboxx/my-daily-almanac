7485c4e11557a4e2ed571e5a3e4be616f6a5f597
Michael Nabatov <happyanddebtfree@gmail.com>
2022-10-12 14:26:38 -0400

    Added bigDataCloudApi function to get Geo location data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,22 @@
-// Crypto API
-// https://rapidapi.com/apidojo/api/investing-cryptocurrency-markets/?utm_source=RapidAPI.com%2Fguides&utm_medium=DevRel&utm_campaign=DevRel
+// this gets the geolocation info of the user
+var userObj = {
+    name: '',
+    city: '',
+    latitude: '',
+    longitude: ''
+}
+
+async function bigDataCloudApi(obj){
+    let request = 'reverse-geocode-client';
+    let resp = {};
+
+    await fetch(`https://api.bigdatacloud.net/data/${request}`)
+        .then(response => response.json())
+        .then(response => {console.log(response); resp = response})
+        .catch(err => console.error(err));
+
+        obj.city = resp.city;    
+        obj.latitude = resp.latitude;    
+        obj.longitude = resp.longitude;  
+}
+

d0119e23148c93f8b9fecc4dfd654f1746e75806
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-10-13 14:18:09 -0400

    added variables and elements for the data I want to pull from the cocktail API

--- assets/js/cocktailContainer.js
+++ assets/js/cocktailContainer.js
@@ -2,16 +2,49 @@ var middle = $('middle')
 
 function renderCocktails(obj){
     console.log(obj);
-    let containerEl
+    let containerEl = $('<div>');
+    //for (var i=0; i<obj.length; i++)
+    containerEl.append("<p>" + obj[i] + "<p>")
+    return containerEl;
+    
 }
 
-//await fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
-//.then(response => response.json())
-//.then(response => resp = response)
-//.catch(err => {console.error(err); return err});
-//console.log(resp);
+var cocktailNameEl = document.createElement("div");
+var cocktailImgEl = document.createElement("img");
+var cocktailIngredientsEl = document.createElement("div");
+var cocktailProportionsEl = document.createElement("div");
+
+h1El.textContent = "Your Daily Cocktail";
+cocktailNameEl.textContent = "";
+cocktailImgEl.
+cocktailIngredientsEl.textContent = "";
+cocktailProportionsEl.textContent = "";
+
+// Add text for list items
+li1.textContent = "";
+li2.textContent = "";
+li3.textContent = "";
+li4.textContent = "";
+
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+var li4 = document.createElement("li");
+
+listEl.appendChild(li1);
+listEl.appendChild(li2);
+listEl.appendChild(li3);
+listEl.appendChild(li4);
+
+
+
+await fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
+.then(response => response.json())
+.then(response => resp = response)
+.catch(err => {console.error(err); return err});
+console.log(resp);
 // obj.cocktail = resp.recipes;  // array
 
-//renderCocktails(obj);
+renderCocktails(resp);
 // save to local storage
 // localStorage.setItem('userObj', JSON.stringify(obj));

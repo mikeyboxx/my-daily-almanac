09e1172276f01f3784e3bf8c43291b8e0f089bfb
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-10-13 15:29:27 -0400

    pulled information from API to populate daily cocktail on screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -120,14 +120,14 @@ async function firstTimeRender(obj){
     renderRecipes(obj);
 
 
-    await fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
-        .then(response => response.json())
-        .then(response => resp = response)
-        .catch(err => {console.error(err); return err});
-    console.log(resp);
-    // obj.cocktail = resp.recipes;  // array
+    // await fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
+    //     .then(response => response.json())
+    //     .then(response => resp = response)
+    //     .catch(err => {console.error(err); return err});
+    // console.log(resp);
+    // // obj.cocktail = resp.recipes;  // array
     
-    renderCocktails(obj);
+    // renderCocktails(obj);
     // save to local storage
     // localStorage.setItem('userObj', JSON.stringify(obj));
 }

1d82e2d06da085fcfe416eb1bb7bb6e476b74b3c
Kathy Shurr <kathyshurr@kathys-mbp.myfiosgateway.com>
2022-10-13 16:20:19 -0400

    added classes to elements, added formatting style.

--- assets/js/horoscope.js
+++ assets/js/horoscope.js
@@ -1,32 +1,35 @@
 function renderHoroscope(obj){
     console.log(obj);
-    let containerEl = $('<div>');
-    // for (var i=0; i< obj.length; i++) {
-        var hEl = $('<h3>').text(userObj.zodiacSign)
+    let containerEl = $('<div>').addClass("horoscope-card");
+
+        var hEl = $('<h3>').text(userObj.zodiacSign + userObj.zodiacIcon).addClass("horoscope-heading");
         containerEl.append(hEl);
 
-        var pEl = $('<p>').text("date-range: " + obj.date_range);
+        var pEl = $('<p>').text(obj.date_range).addClass("horoscope-dates");
+        containerEl.append(pEl);
+        pEl = $('<p>').text("Today's Horoscope:").addClass("todays-horoscope");
         containerEl.append(pEl);
 
-        pEl = $('<p>').text("Compatability: " + obj.compatibility);
+        pEl = $('<p>').text(obj.description).addClass("horoscope");
         containerEl.append(pEl);
 
-        pEl = $('<p>').text("Color: " + obj.color);
+        pEl = $('<p>').text("Compatability: " + obj.compatibility).addClass("horoscope-content");
         containerEl.append(pEl);
 
-        pEl = $('<p>').text("Description: " + obj.description);
+        pEl = $('<p>').text("Color: " + obj.color).addClass("horoscope-content");
         containerEl.append(pEl);
 
-        pEl = $('<p>').text("Lucky Number: " + obj.lucky_number);
+
+        pEl = $('<p>').text("Lucky Number: " + obj.lucky_number).addClass("horoscope-content");
         containerEl.append(pEl);
 
-        pEl = $('<p>').text("Lucky Time: " + obj.lucky_time);
+        pEl = $('<p>').text("Lucky Time: " + obj.lucky_time).addClass("horoscope-content");
         containerEl.append(pEl);
 
-        pEl = $('<p>').text("Mood: " + obj.mood);
+        pEl = $('<p>').text("Mood: " + obj.mood).addClass("horoscope-content");
         containerEl.append(pEl);
 
-    // }
+
     return containerEl;
 
 }

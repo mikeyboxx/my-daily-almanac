dc5d8c9ffa1e341f05b993f2ba69ed54b50920d5
Kathy Shurr <kathyshurr@kathys-mbp.myfiosgateway.com>
2022-10-13 14:38:06 -0400

    added titles to object components displayed. Referenced userobj to create h1 tag at top of the div

--- assets/js/horoscope.js
+++ assets/js/horoscope.js
@@ -2,25 +2,28 @@ function renderHoroscope(obj){
     console.log(obj);
     let containerEl = $('<div>');
     // for (var i=0; i< obj.length; i++) {
-        var pEl = $('<p>').text(obj.color);
+        var hEl = $('<h3>').text(userObj.zodiacSign)
+        containerEl.append(hEl);
+
+        var pEl = $('<p>').text("date-range: " + obj.date_range);
         containerEl.append(pEl);
 
-        pEl = $('<p>').text(obj.compatability);
+        pEl = $('<p>').text("Compatability: " + obj.compatibility);
         containerEl.append(pEl);
 
-        pEl = $('<p>').text(obj.date_range);
+        pEl = $('<p>').text("Color: " + obj.color);
         containerEl.append(pEl);
 
-        pEl = $('<p>').text(obj.description);
+        pEl = $('<p>').text("Description: " + obj.description);
         containerEl.append(pEl);
 
-        pEl = $('<p>').text(obj.lucky_number);
+        pEl = $('<p>').text("Lucky Number: " + obj.lucky_number);
         containerEl.append(pEl);
 
-        pEl = $('<p>').text(obj.lucky_time);
+        pEl = $('<p>').text("Lucky Time: " + obj.lucky_time);
         containerEl.append(pEl);
 
-        pEl = $('<p>').text(obj.mood);
+        pEl = $('<p>').text("Mood: " + obj.mood);
         containerEl.append(pEl);
 
     // }

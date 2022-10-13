e76d92edbf59989466efbd86583a668d88ee4741
Michael Nabatov <happyanddebtfree@gmail.com>
2022-10-13 19:46:21 -0400

    added weatherContaine.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,57 +28,6 @@ function renderWelcomeDialog(obj, fromContainer){
     firstTimeRender(obj);
 }
 
-
-
-function renderWeather(obj){
-    console.log(obj);
-
-    $(weatherCardsContainer).empty();
-    
-    let ctr = 0;
-    let currDt = moment().format('MM/DD');
-    for (let i=0; i<obj.length; i++){
-        let objDt = moment(obj[i].startTime.slice(0,16)).format('MM/DD');
-        let objDtTime = obj[i].startTime.slice(0,16).substr(11,5);
-        if (objDt > currDt && objDtTime === '06:00' && ctr < 5) {
-            ctr++;
-            let colEl = $('<div>').addClass('column is-flex');
-            let cardEl  = $('<div>').addClass('card');
-            let cardHeaderEl  = $('<div>').addClass('card-header');
-            let h2El  = $('<h2>').text(obj[i].name.slice(0,3).toUpperCase());
-            let spanEl  = $('<span>').text(moment(obj[i].startTime.slice(0,16)).format('MM/DD'));
-
-            $(h2El).append(spanEl);
-            $(cardHeaderEl).append(h2El);
-            $(cardEl).append(cardHeaderEl);
-
-            let cardContentEl = $('<div>').addClass('card-content');
-            let pEl = $('<p>').addClass('temp');
-            let iEl = $('<i>').addClass('fa-solid fa-temperature-empty');
-            $(pEl).append(iEl);
-            
-            spanEl = $('<span>').text(obj[i].temperature + '°');
-            $(pEl).append(spanEl);
-            $(cardContentEl).append(pEl);
-
-            pEl = $('<p>').addClass('wind');
-            iEl = $('<i>').addClass('fa fa-wind');
-            $(pEl).append(iEl);
-
-            spanEl = $('<span>').text(obj[i].windSpeed);
-            $(pEl).append(spanEl);
-            $(cardContentEl).append(pEl);
-
-            pEl = $('<p>').addClass('forecast').text(obj[i].shortForecast);
-            $(cardContentEl).append(pEl);
-
-            $(cardEl).append(cardContentEl);
-            $(colEl).append(cardEl);
-            $(weatherCardsContainer).append(colEl);
-        }
-    }
-}
-
 function renderCrypto(obj){
     console.log(obj);
 }
@@ -124,15 +73,15 @@ async function firstTimeRender(obj){
         .then(response => resp = response)
         .catch(err => {console.error(err); return err});
 
-        await fetch(resp.properties.forecast)
+    await fetch(resp.properties.forecast)
         .then(response => response.json())
         .then(response => resp = response)
         .catch(err => {console.error(err); return err});
-        console.log(resp);
-        obj.weather = resp.properties.periods;  // array 2 forecasts per day (i.e. day, night)
-        renderWeather(obj.weather);
+
+    obj.weather = resp.properties.periods;  // array 2 forecasts per day (i.e. day, night)
+    renderWeather(obj.weather);
         
-        await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd`)
+    await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd`)
         .then(response => response.json())
         .then(response => resp = response)
         .catch(err => {console.error(err); return err});
@@ -158,11 +107,9 @@ async function firstTimeRender(obj){
         .then(response => resp = response)
         .catch(err => {console.error(err); return err});
     console.log(resp);
-    // obj.cocktail = resp.recipes;  // array
+     obj.cocktail = resp;  // array
     
     renderCocktails(resp);
-    // save to local storage
-    // localStorage.setItem('userObj', JSON.stringify(obj));
 }
 
 

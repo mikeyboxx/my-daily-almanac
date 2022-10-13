2e8166bf754c6b9e67104b80414f331f5370a947
KC <kevinchen@kevins-air.myfiosgateway.com>
2022-10-12 22:40:53 -0400

    javascript pseudocode textbox content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,17 @@
 // Crypto API
 // https://rapidapi.com/apidojo/api/investing-cryptocurrency-markets/?utm_source=RapidAPI.com%2Fguides&utm_medium=DevRel&utm_campaign=DevRel
+// current day
+var currentDay = moment();
+// if the date is in the past then user can only read notes
+// when user click on edit button a text area will appear
+// if it is present day then user can edit text
+// User will have a save button to save to local storage
+
+function dailyTime() {
+    $("#")
+}
+
+
+
+
+

45513350cc4df932d8f79b250b35bfa5737e31cc
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-10-13 12:46:55 -0400

    starting the cocktail container API

---
+++ assets/js/cocktailContainer.js
@@ -0,0 +1,14 @@
+function renderCocktails(obj){
+    console.log(obj);
+}
+
+//await fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
+//.then(response => response.json())
+//.then(response => resp = response)
+//.catch(err => {console.error(err); return err});
+//console.log(resp);
+// obj.cocktail = resp.recipes;  // array
+
+//renderCocktails(obj);
+// save to local storage
+// localStorage.setItem('userObj', JSON.stringify(obj));

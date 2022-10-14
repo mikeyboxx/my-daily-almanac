457039070ea3525d84e7741665b27e2332a49e5f
Kathy Shurr <kathyshurr@kathys-mbp.myfiosgateway.com>
2022-10-14 11:43:28 -0400

    centered text in horoscope card. Increased size of the heading.

--- assets/js/horoscope.js
+++ assets/js/horoscope.js
@@ -3,31 +3,31 @@ function renderHoroscope(obj){
     let containerEl = $('<div>').addClass(" column is-half");
     let boxEl = $('<div>').addClass("horoscope-card box middle-box ");
 
-        var hEl = $('<h3>').text(userObj.zodiacSign + userObj.zodiacIcon).addClass("horoscope-heading");
+        var hEl = $('<h3>').text(userObj.zodiacIcon + userObj.zodiacSign).addClass("horoscope-heading is-capitalized has-text-centered is-size-3");
         boxEl.append(hEl);
 
-        var pEl = $('<p>').text(obj.date_range).addClass("horoscope-dates");
+        var pEl = $('<p>').text(obj.date_range).addClass("horoscope-dates has-text-centered");
         boxEl.append(pEl);
-        pEl = $('<p>').text("Today's Horoscope:").addClass("todays-horoscope");
-        boxEl.append(pEl);
-
-        pEl = $('<p>').text(obj.description).addClass("horoscope");
+        pEl = $('<p>').text("Today's Horoscope:").addClass("todays-horoscope has-text-centered");
         boxEl.append(pEl);
 
-        pEl = $('<p>').text("Compatability: " + obj.compatibility).addClass("horoscope-content");
+        pEl = $('<p>').text(obj.description).addClass("horoscope has-text-centered");
         boxEl.append(pEl);
 
-        pEl = $('<p>').text("Color: " + obj.color).addClass("horoscope-content");
+        pEl = $('<p>').text("Compatability: " + obj.compatibility).addClass("horoscope-content has-text-centered");
+        boxEl.append(pEl);
+
+        pEl = $('<p>').text("Color: " + obj.color).addClass("horoscope-content has-text-centered");
         boxEl.append(pEl);
 
 
-        pEl = $('<p>').text("Lucky Number: " + obj.lucky_number).addClass("horoscope-content");
+        pEl = $('<p>').text("Lucky Number: " + obj.lucky_number).addClass("horoscope-content has-text-centered");
         boxEl.append(pEl);
 
-        pEl = $('<p>').text("Lucky Time: " + obj.lucky_time).addClass("horoscope-content");
+        pEl = $('<p>').text("Lucky Time: " + obj.lucky_time).addClass("horoscope-content has-text-centered");
         boxEl.append(pEl);
 
-        pEl = $('<p>').text("Mood: " + obj.mood).addClass("horoscope-content");
+        pEl = $('<p>').text("Mood: " + obj.mood).addClass("horoscope-content has-text-centered");
         boxEl.append(pEl);
 
         $(containerEl).append(boxEl);

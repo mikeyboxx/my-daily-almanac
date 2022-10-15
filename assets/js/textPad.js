a6a7cee3c00af51aa85b4ece8138fe9b779ae7e2
Michael Nabatov <happyanddebtfree@gmail.com>
2022-10-15 17:08:44 -0400

    Added logic for Archived Notes and cleaned up the code

--- assets/js/textPad.js
+++ assets/js/textPad.js
@@ -1,52 +1,94 @@
-// current day
-// var currentDay = moment();
-// if the date is in the past then user can only read notes
-// when user click on edit button a text area will appear
-// if it is present day then user can edit text
-// User will have a save button to save to local storage
-// $("#currentDay").text(currentDay.format("MMM Do YYYY"));
-// var notes = $("#notes");
-// var archivedNote = []
-// var userObj = [{
-//     date:currentDay,
-//     text:notes
-// }]
 
-// notes.val(JSON.parse(localStorage.getItem()));
-
-// $("#saveNotes").on("click", function(){
-//     var userInput = notes.val().trim();
-//     console.log(userInput)
-//     localStorage.setItem(date, JSON.stringify(userInput))
-// });
+// to keep track of last menu item selected
+var lastIdx;
 
 function renderTextPad(userObj){
-    console.log('save');
-    var currentDay = moment();
-    var currentDayFormatted =currentDay.format("MMM Do YYYY");
-    console.log(currentDayFormatted);
+    let currentDay = moment();
+    let currentDayFormatted =currentDay.format("MMM Do, YYYY");
     $("#currentDay").text(currentDayFormatted);
 
-    $("#saveNotes").on("click", (function(obj, event){
-        console.log('handler');
-        event.preventDefault();
-        var notes = $("#notes");
-        var userInput = notes.val().trim();
-        console.log(userInput);
-        var note = {
+    let currentDateIdx = userObj.archivedNotes.findIndex((el)=> el.date === currentDayFormatted);
+    //very first time add current date to array 
+    if (currentDateIdx === -1){
+        userObj.archivedNotes.push({
             date: currentDayFormatted,
-            text: userInput
-        };
-        // if(date === currentDayFormatted) {
+            text: ''
+        });
+        lastIdx = userObj.archivedNotes.length -1;
+    } else {
+        $(notes).val(userObj.archivedNotes[currentDateIdx].text);
+        lastIdx = currentDateIdx;
+    }
 
-        // }
+    // build the menu items and event listeners
+    for (let i=0; i < userObj.archivedNotes.length; i++){
+        // if last item which has current date then add a <hr> line
+        if (i === userObj.archivedNotes.length - 1){
+            let hrEl = $('<hr>').addClass('dropdown-divider');
+            $(dropdownContent).append(hrEl);
+        }
 
-        // find the index of current day
-        // if index = -1 (not found)
-          obj.archivedNotes.push(note);
-        // else
-        //    obj.archivedNotes[currentDayidx] = note;
+        // add the index as an attribute
+        let aEl = $('<a>').addClass('dropdown-item').text(userObj.archivedNotes[i].date).attr('item', i);
 
-        localStorage.setItem('userObj', JSON.stringify(obj));
+        // event handler for switching menu items of archived notes
+        $(aEl).on('click', (function(obj, event){
+            event.stopPropagation();
+            saveMsg.textContent = '';
+
+            let currentDay = moment();
+            let currentDayFormatted =currentDay.format("MMM Do, YYYY");
+            
+            let currentDateIdx = obj.archivedNotes.findIndex((el)=>el.date === currentDayFormatted);
+            let currentIdx = Number($(event.target).attr('item')); 
+            
+            // if last item was the current date then save the text in the array
+            if (lastIdx === currentDateIdx && currentIdx !== lastIdx  ){
+                let userInput = $(notes).val().trim();
+                obj.archivedNotes[currentDateIdx].text = userInput;
+            }
+            lastIdx = Number($(event.target).attr('item')); 
+
+            // if it is not current date then make textarea read only
+            if (currentIdx !== currentDateIdx){
+                $(notes).attr('readonly', 'readonly');
+            } else {
+                $(notes).removeAttr('readonly');
+            }
+            $('#currentDay').text(obj.archivedNotes[currentIdx].date);
+            $(notes).val(obj.archivedNotes[currentIdx].text);
+            archivedList.classList.remove("is-active"); 
+        }).bind(this, userObj));   // bind the userObj 
+
+        $(dropdownContent).append(aEl);
+    }
+
+    // handler for save button of text pad area
+    $(saveNotes).on('click', (function(obj, event){
+        event.stopPropagation();
+        let currentDay = moment();
+        let currentDayFormatted =currentDay.format("MMM Do, YYYY");
+
+        let notes = $("#notes");
+        let userInput = notes.val().trim();
+
+        let idx = obj.archivedNotes.findIndex((el)=>el.date === currentDayFormatted); 
+        
+        // if current date note, then save
+        if (idx !== -1){
+            obj.archivedNotes[idx].text = userInput;
+            localStorage.setItem('userObj', JSON.stringify(obj));
+            saveMsg.textContent = 'Your note has been saved!';
+        }
     }).bind(this, userObj));
+
+    // event handler for hiding/showing menu of dropdown itesm
+    $(archivedList).on("click",function(event){
+        event.stopPropagation();
+        if(archivedList.classList.contains("is-active")){
+            archivedList.classList.remove("is-active"); 
+        } else {
+            archivedList.classList.add("is-active");
+        }
+    });
 }
